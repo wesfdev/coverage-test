80b4eb819aaf3c17f6aa989d91c3beec8b7b165d
Wes <wesly_fernandez1@outlook.com>
2024-05-19 11:11:07 -0600

    fix: format

--- lib/coverage_test/validations.ex
+++ lib/coverage_test/validations.ex
@@ -1,6 +1,5 @@
 defmodule CoverageTest.Validations do
-
-  @spec run(String.t) :: {:ok, String.t} | {:error, String.t}
+  @spec run(String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def run(value) do
     if String.length(value) > 0 do
       {:ok, value}
